a6ef59fb4f32fdff2762241f59eb282afbd88d83
zimejin07 <zimejin@gmail.com>
2025-01-18 16:34:53 +0100

    feat(product details): style product details page

---
+++ src/product-details/product-details.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent {
+  productId: number | null = null;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.paramMap.subscribe((params) => {
+      this.productId = Number(params.get('id'));
+    });
+  }
+}
